1e9a455629ac95617c390e3038dcd08bb6002856
Pavel <wol1414@gmail.com>
2021-09-27 09:57:08 +0300

    add modal / Edit icon

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -677,8 +677,8 @@ function eventHandler() {
 		let text2 = logoText.dataset.text;
 		var typed2 = new Typed(logoText, {
 			strings: ['','Agrobankdan'],
-			typeSpeed: 120,
-			backSpeed: 120,
+			typeSpeed: 160,
+			backSpeed: 160,
 			loop: true,
 			showCursor: false
 		});
